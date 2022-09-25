9211c5eb4c598c48f225d68f0ee4cdd985451deb
AlbanSdl <alban.delavoreille@free.fr>
2022-09-25 18:48:07 +0200

    fix: commands can only be used by a server admin

--- src/command.ts
+++ src/command.ts
@@ -10,23 +10,29 @@ config();
 const commands = [
   new SlashCommandBuilder()
     .setName("sync")
-    .setDescription("Effectue la synchronisation des rôles sur le serveur"),
+    .setDescription("Effectue la synchronisation des rôles sur le serveur")
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(0x8),
   new SlashCommandBuilder()
     .setName("update-command")
     .setDescription(
       "Met à jour/Crée le message pour voir les channels des assos"
-    ),
+    )
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(0x8),
   new SlashCommandBuilder()
     .setName("adepte")
     .setDescription(
       "Crée le message pour avoir le rôle Adepte de la salle asso"
-    ),
+    )
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(0x8),
 ].map((command) => command.toJSON());
 
 // Register Commands
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN!);
 
 rest
-  .put(Routes.applicationCommands("1009952506006216794"), { body: commands })
+  .put(Routes.applicationCommands(process.env.BOT_ID), { body: commands })
   .then(() => console.log("Successfully registered application commands."))
   .catch(console.error);
